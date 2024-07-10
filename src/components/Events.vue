45cc3139030f0a1b0fa14e57606e21d7c0ebcfc2
Eternal-insomnia <alexsuvorov2506@gmail.com>
2024-07-10 13:07:57 +0300

    Added tracking of more events
    Added tracking of @click, @click.stop, @input, @change, @keydown, @paste, @mouseover and @mouseleave

--- src/components/Events.vue
+++ src/components/Events.vue
@@ -7,24 +7,90 @@
   <p @click="number=0">Сбросить
     <span @click.stop="">число</span>
   </p>
+
   <br><br>
+  
   <input @input="(event) => message = event.target.value" placeholder="Введите что-нибудь"><br>
   <p v-if="message==''">Тут пусто...</p>
-  <p v-else>{{ message }}</p><br>
+  <p v-else>{{ message }}</p>
+  
+  <br><br>
+  
+  <select @change="selected">
+    <option value="" selected disabled>Выберите</option>
+    <option value="A">A</option>
+    <option value="B">B</option>
+    <option value="C">C</option>
+  </select><br>
+  <p>Ваш выбор: {{ choice }}</p>
+
+  <br><br>
+
+  <p :class="keyDownClass">Введите текст и нажмите Enter</p><br>
+  <input @keydown.enter="enterKeyDown">
+  
+  <br><br>
+
+  <p>Вы можете вставить сюда текст из буфера обмена:</p><br>
+  <textarea @paste="onPaste"></textarea><br>
+  <p>{{ pastedText }}</p>
+
+  <br><br>
+
+  <p :class="coolTextClass" @mouseover="coolTextChange" @mouseleave="coolTextChange">{{ coolText }}</p>
 </template>
 
 <script>
 export default {
   data() {
     return {
+      choice: "Вы ничего не выбрали.",
+      coolText: "Я красивый текст",
+      coolTextClass: "mouseLeave",
+      keyDownClass: "",
+      message: "Тут пусто.",
       number: 0,
-      message: "Тут пусто."
+      pastedText: ""
     }
   },
   methods: {
+    coolTextChange() {
+      if (this.coolTextClass == "mouseLeave") {
+        this.coolTextClass = "mouseOver"
+        this.coolText = "НЕЕЕЕЕЕЕТ, НЕ ТРОГАЙ"
+      } else {
+        this.coolTextClass = "mouseLeave"
+        this.coolText = "Я красивый текст"
+      }
+    },
+    enterKeyDown() {
+      this.keyDownClass = "keyDown"
+    },
     increment() {
       this.number++
+    },
+    onPaste(event) {
+      this.pastedText = event.clipboardData.getData('text')
+    },
+    selected(event) {
+      this.choice = event.target.value
     }
   }
 }
 </script>
+
+<style>
+.keyDown {
+  color: green;
+  background: greenyellow;
+}
+
+.mouseOver {
+  color: #ff0000;
+  background: #651b1b;
+}
+.mouseLeave {
+  color:rgb(0, 174, 255);
+  background: rgb(0, 65, 103);
+}
+</style>
